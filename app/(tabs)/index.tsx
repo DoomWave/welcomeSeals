bc13b8ac8d88b043ddc622a9ac4d179f787d8e3e
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-03-19 15:27:47 -0700

    Project finished it, I think.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
-import { Image, StyleSheet, Text,View, ImageBackground, TextInput } from 'react-native';
-import React, { useCallback } from 'react';
+import { Image, StyleSheet, Text,View, ImageBackground, TextInput, SafeAreaView, Button } from 'react-native';
+import React, { useCallback, useState } from 'react';
 import { useFonts } from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen';
+import Modal from 'react-native-modal';
 
 export default function HomeScreen() {
   const [fontsLoaded] = useFonts({
     'stencilla': require('@/assets/fonts/stencilla.ttf'),
   });
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  }
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -17,6 +23,13 @@ export default function HomeScreen() {
   if (!fontsLoaded){
     return null;
   }
+
+  const [inputText, setInputText] = useState('');
+
+  const handleSave = () => {
+    onSave(inputText);
+    setInputText(''); // Clear input after saving
+  };
   const [text, onChangeText] = React.useState('Ready to join?');
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
@@ -26,21 +39,24 @@ export default function HomeScreen() {
           width: 200,
           height: 200,
           borderRadius: 100,
-          alignSelf: 'center'
+          alignSelf: 'center',
+          borderColor: '#00218E',
+          borderWidth: 5
         }}
       />
       <Text style={styles.sealName}>SGT. Seal D. Goggings</Text>
       <Text style={styles.Age}>24 Years old</Text>
-      <Text style={styles.description}>For the Seal D. Goggins, childhood was a nightmare -- poverty, prejudice, and physical abuse colored his days and haunted his nights.
+      <Text style={styles.description}>For the Seal D. Goggins, childhood was a nightmare -- poverty, prejudice,<b/> and physical abuse colored his days and haunted his nights.
          But through self-discipline, mental toughness, and hard work, Goggins transformed himself from a depressed, overweight young man with no future into a U.S. Armed Forces icon and one of the world's top endurance athletes. 
          The only man in history to complete elite training as a Navy SEAL, Army Ranger, and Air Force Tactical Air Controller, 
          he went on to set records in numerous endurance events, 
          inspiring Outside magazine to name him "The Fittest (Real) Man in America."</Text>
-         <TextInput
-         style={styles.input}
-         onChangeText={onChangeText}
-         value={text}
-         />
+        <TextInput
+            style={styles.input}
+            onChangeText={setInputText}
+            value={inputText}
+            placeholder="Input here"
+          />
     </View>
 
   );
@@ -51,7 +67,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'blue',
+    backgroundColor: '#0D1F5E',
   },
  sealName: {
   color: 'white',
@@ -64,16 +80,27 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     color: 'white',
     backgroundColor: 'black',
-    fontFamily: "stencilla"
+    fontFamily: "stencilla",
+    textAlignVertical : 'center'
   },
   Age: {
     color: 'white',
+    fontFamily: "stencilla"
   },
   input: {
     height: 20,
     margin: 10,
     borderWidth: 1,
     padding: 5,
-    fontFamily: "stencilla"
+    fontFamily: "stencilla",
+    backgroundColor: 'white'
+  },
+  bottomModal: {
+    justifyContent: 'flex-end',
+    margin: 0,
   },
 });
+function onSave(inputText: string) {
+  throw new Error('Function not implemented.');
+}
+
