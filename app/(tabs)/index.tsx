76a1a33160d7d4ea6ed30b7d60b3f1bb7aa5ca67
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-03-18 18:44:03 -0700

    text input added.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
-import { Image, StyleSheet, Text,View, ImageBackground } from 'react-native';
-
+import { Image, StyleSheet, Text,View, ImageBackground, TextInput } from 'react-native';
+import React from 'react';
 
 export default function HomeScreen() {
+  const [text, onChangeText] = React.useState('Useless Text');
   return (
     <View style={{backgroundColor: "blue", flex: 1}}>
       <Image
@@ -14,12 +15,19 @@ export default function HomeScreen() {
         }}
       />
       <Text style={styles.sealName}>SGT. Seal D. Goggings</Text>
+      <Text style={styles.Age}>24 Years old</Text>
       <Text style={styles.description}>For David Goggins, childhood was a nightmare -- poverty, prejudice, and physical abuse colored his days and haunted his nights.
          But through self-discipline, mental toughness, and hard work, Goggins transformed himself from a depressed, overweight young man with no future into a U.S. Armed Forces icon and one of the world's top endurance athletes. 
          The only man in history to complete elite training as a Navy SEAL, Army Ranger, and Air Force Tactical Air Controller, 
          he went on to set records in numerous endurance events, 
          inspiring Outside magazine to name him "The Fittest (Real) Man in America."</Text>
+         <TextInput 
+         style={styles.input}
+         onChangeText={onChangeText}
+         value={Text}
+         />
     </View>
+
   );
 }
 
@@ -27,12 +35,22 @@ const styles = StyleSheet.create({
  sealName: {
   color: 'white',
    alignSelf: 'center',
-   fontSize: '40px'},
+   fontSize: '40px',
+  fontFamily: ""
+  },
   
   description: {
     alignSelf: 'center',
     color: 'white',
-
-  }
-
+    backgroundColor: 'black',
+  },
+  Age: {
+    color: 'white',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
