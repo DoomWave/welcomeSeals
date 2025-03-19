b337c8e13977176ab03549ee376914a8e10b84cf
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-03-19 14:48:23 -0700

    Font added.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,25 @@
 import { Image, StyleSheet, Text,View, ImageBackground, TextInput } from 'react-native';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    'stencilla': require('@/assets/fonts/stencilla.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded){
+    return null;
+  }
   const [text, onChangeText] = React.useState('Ready to join?');
   return (
-    <View style={{backgroundColor: "blue", flex: 1}}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Image
         source={require('@/assets/images/pfp.jpg')}
         style={{
@@ -21,7 +36,7 @@ export default function HomeScreen() {
          The only man in history to complete elite training as a Navy SEAL, Army Ranger, and Air Force Tactical Air Controller, 
          he went on to set records in numerous endurance events, 
          inspiring Outside magazine to name him "The Fittest (Real) Man in America."</Text>
-         <TextInput 
+         <TextInput
          style={styles.input}
          onChangeText={onChangeText}
          value={text}
@@ -32,17 +47,24 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+  },
  sealName: {
   color: 'white',
    alignSelf: 'center',
    fontSize: 40,
-   fontFamily: ""
+   fontFamily: "stencilla"
   },
   
   description: {
     alignSelf: 'center',
     color: 'white',
     backgroundColor: 'black',
+    fontFamily: "stencilla"
   },
   Age: {
     color: 'white',
@@ -52,6 +74,6 @@ const styles = StyleSheet.create({
     margin: 10,
     borderWidth: 1,
     padding: 5,
-
+    fontFamily: "stencilla"
   },
 });
