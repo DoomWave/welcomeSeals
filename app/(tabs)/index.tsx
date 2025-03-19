ed60b4ec3faed7db9b60adee43e575029def4a5c
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-03-18 17:24:19 -0700

    description added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Image, StyleSheet, Text,View, ImageBackground } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
+    <View style={{backgroundColor: "blue", flex: 1}}>
       <Image
         source={require('@/assets/images/pfp.jpg')}
         style={{
@@ -13,12 +13,26 @@ export default function HomeScreen() {
           alignSelf: 'center'
         }}
       />
-      <Text style={{color: 'white', alignSelf: 'center'}}>SGT. Seal D. Goggings</Text>
-      <Text>???</Text>
+      <Text style={styles.sealName}>SGT. Seal D. Goggings</Text>
+      <Text style={styles.description}>For David Goggins, childhood was a nightmare -- poverty, prejudice, and physical abuse colored his days and haunted his nights.
+         But through self-discipline, mental toughness, and hard work, Goggins transformed himself from a depressed, overweight young man with no future into a U.S. Armed Forces icon and one of the world's top endurance athletes. 
+         The only man in history to complete elite training as a Navy SEAL, Army Ranger, and Air Force Tactical Air Controller, 
+         he went on to set records in numerous endurance events, 
+         inspiring Outside magazine to name him "The Fittest (Real) Man in America."</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- 
+ sealName: {
+  color: 'white',
+   alignSelf: 'center',
+   fontSize: '40px'},
+  
+  description: {
+    alignSelf: 'center',
+    color: 'white',
+
+  }
+
 });
